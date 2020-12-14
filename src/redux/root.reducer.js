c859bf80d6cadf29d13b7595bdb0385a9f79ec2d
abhinavg247 <abhinavg247@gmail.com>
2020-12-14 20:38:44 +0530

    moved our directory and shop data into its own reducer, created respective selectors and corresponding components with new redux flow for directory/shop

--- src/redux/root.reducer.js
+++ src/redux/root.reducer.js
@@ -1,9 +1,24 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // local storage of our window
+// can also import sessionStorage from 'redux-persist/... if we want to use session storage instead
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
+import directoryReducer from './directory/directory.reducer';
+import shopReducer from './shop/shop.reducer';
 
-export default combineReducers({
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart'],
+};
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
+  directory: directoryReducer,
+  shop: shopReducer,
 });
+
+export default persistReducer(persistConfig, rootReducer);
